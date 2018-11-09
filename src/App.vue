61a5ccef75cbd2876a341472a896c0dd412682fb
hht8011@163.com <hht8011@163.com>
2018-11-09 15:41:55 +0800

    解决状态报错头部无法显示问题

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
         <transition name="router-fade" mode="out-in">
             <router-view v-if="!$route.meta.keepAlive"></router-view>
         </transition>
+        <svg-icon></svg-icon>
     </div>
 </template>
 
@@ -19,13 +20,14 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import  './style/common';
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
 </style>
--- src/config/rem.js
+++ src/config/rem.js
@@ -2,7 +2,7 @@
     var docEl = doc.documentElement,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function() {
-            var clientWidth = docEl.clientWidth;
+            var clientWidth = docEl.clientWidth>640 ?640:docEl.clientWidth;
             if (!clientWidth) return;
             docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
         };
